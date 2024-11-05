0211e0b1e329e86be5e2022a96e539a86f72fde5
FearlessYak5088 <sunfan2266@gmail.com>
2024-11-05 11:05:03 -0500

    Update fan_game.py
    Set screen size, some variables, started loop

--- fan_game.py
+++ fan_game.py
@@ -1,1 +1,31 @@
+# Avoid The Obstacles
 
+import random
+import pygame
+from pygame.locals import K_ESCAPE, QUIT, K_RIGHT, K_LEFT
+
+pygame.init()
+
+# Set Display
+WIDTH = 600
+HEIGHT = 800
+screen = pygame.display.set_mode(WIDTH, HEIGHT)
+pygame.display.set_caption("Avoid the Obstacles by Fan")
+
+current_screen = 0
+running = True
+
+# Colour Variables
+black = (0, 0, 0)
+white = (255, 255, 255)
+gray = (128, 128, 128)
+
+# Loop
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+        elif event.key == K_ESCAPE:
+            running = False
+    if current_screen == 0:
+        screen.fill(black)
