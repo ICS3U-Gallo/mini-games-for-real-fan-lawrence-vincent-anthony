4badde71a9e2671d72d00dee65f6d2ac594b987f
seekseeklest <fluffycloud2008@gmail.com>
2024-11-06 20:35:39 -0500

    Update larry_game.py

--- larry_game.py
+++ larry_game.py
@@ -1,25 +1,48 @@
-import pygame
+import pygame, sys
 import pygame.freetype 
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((1500, 1500))
 clock = pygame.time.Clock()
 
 sky_blue = (158, 240, 255)
 
-GAME_FONT = pygame.freetype.Font(None, 24)
-running =  True
+AgmenaPro = 'Agmena Pro Book.ttf'
+
+GAME_FONT = pygame.freetype.Font(AgmenaPro, 14)
+
+dialogue_lines = [
+    "Light.",
+    "Underneath the suffocating soil, you can make out a light gleaming from above.",
+    "Your bones creak as you burrow up, an ode to your antiquity.",
+    "The light becomes brighter the further up you crawl.",
+    "Finally, you escape the earth's confines.",
+    "An atrocious landscape greets you.",
+    "The year is 2078.",
+    "Due to corporate greed and intercontinental warfare, the earth has become a deserted wasteland.",
+    "Far too many cow farts in the ozone layer have trapped the Sun's immense heat onto earth, making survival a near impossible feat for the earth dwellers."
+]
+current_line = 0
+
+running = True
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                if current_line < len(dialogue_lines) - 1:
+                    current_line += 1
+                else:
+                    current_line = 0
 
     screen.fill(sky_blue)
-    GAME_FONT.render_to(screen, (300, 275), "Welcome to Sun Quest!", (0, 0, 0))
+
+    GAME_FONT.render_to(screen, (100, 275), dialogue_lines[current_line], (0, 0, 0))
 
     pygame.display.flip()
-    clock.tick(60)/1000
+    clock.tick(60)
 
 pygame.quit()
