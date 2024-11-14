ce6462f30caf5e946fab32a0b95db1c0c3fd8c15
Anthony3245 <anthony.yu26@ycdsbk12.ca>
2024-11-13 20:14:43 -0500

    Update anthony_game.py
    Added a multiball power up

--- anthony_game.py
+++ anthony_game.py
@@ -10,11 +10,11 @@ screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
 
 BALL_RADIUS = 8
-ball_speed_x = 4
-ball_speed_y = -4
 ball_speed_x = 7
 ball_speed_y = -7
-ball = pygame.Rect(WIDTH // 2, HEIGHT // 2, BALL_RADIUS * 2, BALL_RADIUS * 2)
+
+balls = [pygame.Rect(WIDTH // 2, HEIGHT // 2, BALL_RADIUS * 2, BALL_RADIUS * 2)]
+ball_speeds = [(ball_speed_x, ball_speed_y)]
 
 paddle_width = 80
 paddle_height = 10
@@ -25,11 +25,15 @@ BRICK_HEIGHT = 20
 initial_bricks = 9 * 5
 bricks = [pygame.Rect(10 + i * (BRICK_WIDTH + 10), 10 + j * (BRICK_HEIGHT + 10), BRICK_WIDTH, BRICK_HEIGHT)
           for i in range(9) for j in range(5)]
+
 stars = []
 for _ in range(100):
     x = random.randrange(0, WIDTH)
     y = random.randrange(0, HEIGHT)
     stars.append((x, y))
+
+first_row_bricks = [brick for brick in bricks if brick.y == 10]
+multi_ball_box = random.choice(first_row_bricks)
 
 score = 0
 font = pygame.font.Font(None, 36)
@@ -43,25 +47,38 @@ while running:
             running = False
 
     if not game_over and not win:
-        ball.x += ball_speed_x
-        ball.y += ball_speed_y
+        for i, ball in enumerate(balls):
+            ball.x += ball_speeds[i][0]
+            ball.y += ball_speeds[i][1]
 
-        if ball.left <= 0 or ball.right >= WIDTH:
-            ball_speed_x *= -1
-        if ball.top <= 0:
-            ball_speed_y *= -1
-        if ball.bottom >= HEIGHT: 
-            game_over = True  
-
-        if ball.colliderect(paddle):
-            ball_speed_y *= -1
-
-        for brick in bricks[:]:
-            if ball.colliderect(brick):
-                bricks.remove(brick)
-                ball_speed_y *= -1
-                score += 1
+            if ball.left <= 0 or ball.right >= WIDTH:
+                ball_speeds[i] = (-ball_speeds[i][0], ball_speeds[i][1])
+            if ball.top <= 0:
+                ball_speeds[i] = (ball_speeds[i][0], -ball_speeds[i][1])
+            if ball.bottom >= HEIGHT:
+                balls.pop(i)
+                ball_speeds.pop(i)
+                if len(balls) == 0:
+                    game_over = True
                 break
+
+            if ball.colliderect(paddle):
+                ball_speeds[i] = (ball_speeds[i][0], -ball_speeds[i][1])
+
+            for brick in bricks[:]:
+                if ball.colliderect(brick):
+                    bricks.remove(brick)
+                    ball_speeds[i] = (ball_speeds[i][0], -ball_speeds[i][1])
+                    score += 1
+
+                    if brick == multi_ball_box:
+                        for _ in range(2):
+                            new_ball = pygame.Rect(ball.x, ball.y, BALL_RADIUS * 2, BALL_RADIUS * 2)
+                            new_speed_x = random.choice([-7, 7])
+                            new_speed_y = random.choice([-7, 7])
+                            balls.append(new_ball)
+                            ball_speeds.append((new_speed_x, new_speed_y))
+                    break
 
         if not bricks:
             win = True
@@ -70,41 +87,38 @@ while running:
         paddle.centerx = mouse_x
         paddle.clamp_ip(screen.get_rect())
 
-        screen.fill((0, 0, 0)) 
-        pygame.draw.rect(screen, (225, 225, 225), paddle) 
-        pygame.draw.ellipse(screen, (0, 225, 0), ball) 
-
-        # Calculate background color based on the number of bricks remaining
         day_progress = 255 * (1 - len(bricks) / initial_bricks)
         background_color = (day_progress // 2, day_progress // 2, day_progress)
-        sun_y = HEIGHT - int((day_progress / 255) * HEIGHT)
-   
         screen.fill(background_color)
+
+        sun_y = HEIGHT - int((day_progress / 255) * HEIGHT)
         pygame.draw.circle(screen, (255, 223, 0), (WIDTH // 2, sun_y), 50)
-        pygame.draw.rect(screen, (225, 225, 225), paddle)
-        pygame.draw.ellipse(screen, (0, 225, 0), ball)
-        star_alpha = max(0, 255 - day_progress)  
+
+        star_alpha = max(0, 255 - day_progress)
         for x, y in stars:
-            pygame.draw.rect(screen, (255, 255, 255), (x, y, 2, 2))
             star_surface = pygame.Surface((2, 2))
             star_surface.fill((255, 255, 255))
             star_surface.set_alpha(star_alpha)
             screen.blit(star_surface, (x, y))
 
+        for ball in balls:
+            pygame.draw.ellipse(screen, (225, 225, 0), ball)
+
+        pygame.draw.rect(screen, (225, 225, 225), paddle)
+
         for brick in bricks:
-            pygame.draw.rect(screen, (225, 0, 0), brick)
+            color = (0, 0, 255) if brick == multi_ball_box else (225, 0, 0)
+            pygame.draw.rect(screen, color, brick)
 
         score_text = font.render(f"Score: {score}", True, (255, 255, 255))
-        screen.blit(score_text, (10, 10))
+        screen.blit(score_text, (5, 5))
 
     else:
         screen.fill((0, 0, 0))
         if game_over:
-            end_text = font.render("Game Over!", True, (255, 0, 0))
             end_text = font.render("Game Over! You are unable to free the Sun", True, (255, 0, 0))
         elif win:
-            end_text = font.render("You Win!", True, (0, 255, 0))
-            end_text = font.render("You Win! You have freed the Sun! ", True, (0, 255, 0))
+            end_text = font.render("You Win! You have freed the Sun!", True, (0, 255, 0))
         screen.blit(end_text, (WIDTH // 2 - end_text.get_width() // 2, HEIGHT // 2 - end_text.get_height() // 2))
 
     pygame.display.flip()
